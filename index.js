1c457209ca22e3ea038ee8eba88ec69c4f4803f2
amrgharz <ammor.ghr@gmail.com>
2018-03-06 11:31:03 +0200

    first-commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require ('express')
 const app = express ()
-app.get('/' , (req , res) => res.send('batata'))
+app.get('/' , (req , res) => res.send('batata batata'))
 app.listen(3000 , ()=>console.log('example 3000 :)'))
